d1d4007de0f717ae5579501c010e28b744e7ca48
siddhesh <siddhesh.shetye@neosofttech.com>
2012-12-22 14:02:47 +0530

    added comments and changed variable name

--- src/com/sid/fragmentdialog/ViewDialogFragment.java
+++ src/com/sid/fragmentdialog/ViewDialogFragment.java
@@ -10,15 +10,40 @@ import android.view.Window;
 
 import com.sid.dialoginterface.ViewDialogInterface;
 
-
+/**
+ * The Class ViewDialogFragment.
+ * This clas gives a fragment dialog with users custome view
+ * @author Siddhesh
+ */
 public class ViewDialogFragment extends DialogFragment{
-	
+
+	/** The m context. */
 	static Context mContext;
+	
+	/** The ViewDialogInterface object */
 	static ViewDialogInterface interfaceDialog;
-	boolean mTitleBar;
+	
+	/** The title bar flag. */
+	boolean isTitleBarVisible;
+	
+	/** The title bar text. */
 	String mTitle;
+	
+	/** The anim for dialog. */
 	Integer mAnim;
-public static ViewDialogFragment newInstance(Context ctx,Integer view,int style,Integer theme,ViewDialogInterface viewDialogInterface,boolean cancelable) {
+	
+	/**
+	 * New instance.
+	 *
+	 * @param ctx the ctx
+	 * @param view the view
+	 * @param style the style
+	 * @param theme the theme
+	 * @param viewDialogInterface the view dialog interface
+	 * @param cancelable the cancelable
+	 * @return the view dialog fragment
+	 */
+	public static ViewDialogFragment newInstance(Context ctx,Integer view,int style,Integer theme,ViewDialogInterface viewDialogInterface,boolean cancelable) {
 		mContext=ctx;
 		interfaceDialog=viewDialogInterface;
 		Bundle args = new Bundle();
@@ -27,45 +52,68 @@ public static ViewDialogFragment newInstance(Context ctx,Integer view,int style,
 		args.putInt("style", style);
 		ViewDialogFragment frag = new ViewDialogFragment();
 		frag.setArguments(args);
-		
+
 		return frag;
-		
+
 	}
 
 
-public static ViewDialogFragment newInstance(Context ctx,Integer view,ViewDialogInterface viewDialogInterface) {
-	mContext=ctx;
-	interfaceDialog=viewDialogInterface;
-	Bundle args = new Bundle();
-	args.putInt("view", view);
-	ViewDialogFragment frag = new ViewDialogFragment();
-	frag.setArguments(args);
-	
-	return frag;
-	
-}
-	
+	/**
+	 * New instance.
+	 *
+	 * @param ctx the ctx
+	 * @param view the view
+	 * @param viewDialogInterface the view dialog interface
+	 * @return the view dialog fragment
+	 */
+	public static ViewDialogFragment newInstance(Context ctx,Integer view,ViewDialogInterface viewDialogInterface) {
+		mContext=ctx;
+		interfaceDialog=viewDialogInterface;
+		Bundle args = new Bundle();
+		args.putInt("view", view);
+		ViewDialogFragment frag = new ViewDialogFragment();
+		frag.setArguments(args);
+
+		return frag;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		if(!mTitleBar)
-			getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+		if(!isTitleBarVisible)
+				getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
 		else if(mTitle!=null)
-			getDialog().setTitle(mTitle);	
+				getDialog().setTitle(mTitle);	
 		if(mAnim!=null)
-		getDialog().getWindow().getAttributes().windowAnimations=mAnim;
+				getDialog().getWindow().getAttributes().windowAnimations=mAnim;
 		Bundle bundle=getArguments();
 		int v=bundle.getInt("view");
 		View view=inflater.inflate(v, null);
 		if(interfaceDialog!=null)
-		interfaceDialog.getView(view);
+			interfaceDialog.getView(view);
 		return view;
 	}
-	
+
+	/**
+	 * Sets the dialog title.
+	 *
+	 * @param isTitleBarVisible the is title bar visible
+	 * @param title the title
+	 */
 	public void setDialogTitle(boolean isTitleBarVisible,String title){
-		mTitleBar=isTitleBarVisible;
+		this.isTitleBarVisible=isTitleBarVisible;
 		mTitle=title;
 	}
+	
+	/**
+	 * Sets the dialog animation.
+	 *
+	 * @param animation the new dialog animation
+	 */
 	public void setDialogAnimation(Integer animation){
 		mAnim=animation;
 	}
